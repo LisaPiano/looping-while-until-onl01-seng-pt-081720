00ce29a331740970254f8a255fd5ff39f8739626
Lisa Walters <campi_l@fortlewis.edu>
2020-08-20 08:14:03 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,6 @@
 def using_while
   levitation_force = 6
-  while levitation_force < 20
+  while levitation_force < 10
     puts "Wingardium Leviosa"
     levitation_force += 1
 end
@@ -8,7 +8,7 @@ end
 
 def using_until
   levitation_force = 6
-  until levitation_force == 20
+  until levitation_force == 10
     puts "Wingardium Leviosa"
     levitation_force += 1
   end
